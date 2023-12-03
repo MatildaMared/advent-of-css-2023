b8b8b4a87dc99027c84f19510dac7493d1117243
Matilda Mared <matildamared@live.se>
2023-12-03 15:25:16 +0100

    add fonts

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      condensed: ['Bebas Neue', 'sans-serif'],
+      handwriting: ['Kalam', 'cursive'],
+      sans: ['Inter', 'sans-serif'],
+      script: ['Agbalumo', 'cursive'],
+    },
   },
   plugins: [],
 }
-
